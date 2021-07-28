d46b6c9d940999571b7a1b680df3809fad61cd6d
srijan-git <srijan.khan20@gmail.com>
2021-07-28 14:44:21 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const multer = require('multer')
-const cros = require('cors')
+const cors = require('cors')
 const mongoose = require('mongoose');
 require('dotenv').config()
 const homeRouter = require('./Router/router')
