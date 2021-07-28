074e6437b9a823f65c97b3bd577aaaf63602fef5
srijan-git <srijan.khan20@gmail.com>
2021-07-28 14:48:02 +0530

    make it better

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Credentials", "true");s
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 
