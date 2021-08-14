3ddd38fc936ef56f26a22a510633ee1e8debb3d0
srijan-git <srijan.khan20@gmail.com>
2021-08-14 10:02:45 +0530

    make it better

--- Controller/Admin.Controller.js
+++ Controller/Admin.Controller.js
@@ -1,17 +1,21 @@
 const ProductModel = require("../Model/AdminProductModel")
+
+//-----------------Connect to the cloudinary server to Store The images----------//
 const cloudinary = require('cloudinary').v2
 cloudinary.config({
     cloud_name: 'dd1rtlezl',
     api_key: '472355321544534',
     api_secret: 'c7b7Lajl9Pt1K00JkMzmon9GxJE'
 });
+//-----------------Connect to the cloudinary server to Store The images----------//
+
 
 exports.getProductName = (req, res) => {
     res.render('Admin/addProduct')
 }
 
 
-
+//--------------------AddProduct---------------//
 exports.postProduct = (req, res) => {
     console.log(req.body);
     const title = req.body.title
@@ -44,7 +48,10 @@ exports.postProduct = (req, res) => {
         })
     })
 }
+//--------------------AddProduct---------------//
 
+
+//--------------------Product Edit---------------//
 
 exports.postproductEdit = (req, res) => {
     const pId = req.body.pId
@@ -93,9 +100,11 @@ exports.postproductEdit = (req, res) => {
     })
 
 }
+//--------------------Product Edit---------------//
 
 
 
+//--------------------Get The Added Products---------------//
 exports.getproductData = (req, res) => {
     ProductModel.find().then((product) => {
         console.log(product)
@@ -112,10 +121,10 @@ exports.getproductData = (req, res) => {
         })
     })
 }
+//--------------------Get The Added Products---------------//
 
 
-
-
+//--------------------Get The Edited Products---------------//
 exports.getEditedProduct = (req, res) => {
     //To collect the product id form URL
     const pId = req.params.pId
@@ -134,10 +143,11 @@ exports.getEditedProduct = (req, res) => {
         })
     })
 }
+//--------------------Get The Edited Products---------------//
 
 
 
-
+//------------------Deleting The data----------------//
 exports.productDelete = (req, res) => {
     const pId = req.params.pId
     ProductModel.deleteOne({ _id: pId }).then(result => {
@@ -154,3 +164,4 @@ exports.productDelete = (req, res) => {
         })
     })
 }
+//------------------Deleting The data----------------//
